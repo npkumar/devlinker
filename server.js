a9266fc5cfca40f57e031620bed1312d0a89887a
Nitin <nitin@smove.sg>
2018-05-05 17:21:47 +0800

    Connect to mLab via mongoose

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const port = process.env.PORT || 5000;
+
+// DB Configuration
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB connected!'))
+  .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('NPK!'));
 
