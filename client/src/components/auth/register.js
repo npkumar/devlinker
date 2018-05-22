677d6ba8513e1dbf4a83bdfb47eccd9ed378a28c
Nitin <nitin@smove.sg>
2018-05-22 09:33:48 +0800

    Set up proxy correctly and axios

--- client/src/components/auth/register.js
+++ client/src/components/auth/register.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class Register extends Component {
 
@@ -30,7 +31,14 @@ class Register extends Component {
       password: this.state.password,
       confirmPassword: this.state.confirmPassword
     }
-    console.log(newUser);
+
+    axios.post('/api/users/register', newUser)
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => {
+        console.log(err.response.data)
+      })
   }
 
   render() {
