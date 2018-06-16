22b0964eecbf4045d2b10218233909a1d3b396f3
Nitin <nitin@smove.sg>
2018-06-16 21:14:11 +0800

    Add various input field groups

---
+++ client/src/components/common/SelectListGroup.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import classnames from 'classnames';
+import { PropTypes } from 'prop-types';
+
+const SelectListGroup = ({
+  name,
+  value,
+  error,
+  info,
+  onChange,
+  options
+}) => {
+  const selectOptions = options.map(option => (
+    <option key={option.label} value={option.value}>
+      {option.label}
+    </option>
+  ));
+  return (
+    <div noValidate className="form-group">
+      <select
+        value={value}
+        onChange={onChange}
+        className={classnames("form-control form-control-lg", {
+          'is-invalid': error
+        })}
+        name={name}
+        >
+        { selectOptions }
+      </select>
+        { info && <small className="form-text text-muted">{ info }</small>}
+        { error && <div className="invalid-feedback">{ error }</div> }
+    </div>
+  )
+}
+
+SelectListGroup.propTypes = {
+  name: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  value: PropTypes.string.isRequired,
+  info: PropTypes.string,
+  error: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  options: PropTypes.array.isRequired
+};
+
+export default SelectListGroup;
